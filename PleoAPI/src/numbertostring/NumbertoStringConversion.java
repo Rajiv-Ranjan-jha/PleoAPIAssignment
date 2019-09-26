b32e403527e0f42e04160e0dc6e959b61c07ae30
Rajiv <rajivjha2014@gmail.com>
2019-09-26 16:00:40 +0530

    Basic REST to return a number its getting passed in get call

--- PleoAPI/src/numbertostring/NumbertoStringConversion.java
+++ PleoAPI/src/numbertostring/NumbertoStringConversion.java
@@ -1,5 +1,19 @@
 package numbertostring;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/NumberToString")
 public class NumbertoStringConversion {
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String NumbertoStringHTML(@QueryParam("number") String number) {
+		
+		return number;
+	}
 
 }
