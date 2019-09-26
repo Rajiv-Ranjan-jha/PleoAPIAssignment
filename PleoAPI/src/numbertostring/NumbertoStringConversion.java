ea22858764797b18f6a292cd830eae523cff89bd
Rajiv <rajivjha2014@gmail.com>
2019-09-26 16:50:48 +0530

    Replacing from command separated formate to Space separated formate

--- PleoAPI/src/numbertostring/NumbertoStringConversion.java
+++ PleoAPI/src/numbertostring/NumbertoStringConversion.java
@@ -15,6 +15,11 @@ public class NumbertoStringConversion {
 		Double numParsed = Double.parseDouble(number);
 		/* Converting into required formate and rounding off the last two digit post decimal */
 		String result = String.format("%,.2f", numParsed);
+		
+		/* Replacing "," with spaces as its required that formate */
+		if(result.indexOf(",")!=-1) { 
+			result = result.replaceAll(","," "); 
+			}
 		String resource = "<h1> Here is the string Formate of number "+result+" </h1>";
 		return resource;
 		
